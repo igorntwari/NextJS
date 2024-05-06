d37bb659c20f0409228d99ccd5b6a826d7f8ed6b
igorntwari <igorntwari28@gmail.com>
2024-05-06 14:34:26 +0200

    fea:added new codes

--- egreen/src/pages/LandingPage.tsx
+++ egreen/src/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import LandingPicture from "../assets/pictures/LandingPicture.jpg";
@@ -8,6 +10,21 @@ import SmallLandingPicture from "../assets/pictures/SmallLandingPicture.jpg";
 import Navigate from "@/assets/Icons/Navigate";
 import Search from "@/assets/Icons/Search";
 export default function LandingPage() {
+  const [where, setWhere] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [who, setWho] = useState("");
+
+  const handleSearch = () => {
+    console.log(
+      `Where: ${where}, Check In: ${checkIn}, Check Out: ${checkOut}, Who: ${who}`
+    );
+    setWhere("");
+    setCheckIn("");
+    setCheckOut("");
+    setWho("");
+  };
+
   return (
     <div className="relative h-screen">
       <Image
@@ -51,9 +68,11 @@ export default function LandingPage() {
             <br /> both rent and sale, and allow us to help you discover the
             perfect <br /> place for you to call home sweet home.
           </p>
-          <button className="bg-[#FFDBA0] py-2 px-4 rounded-3xl font-Manrope-semiBold text-black">
-            Find a house
-          </button>
+          <Link href="/products">
+            <button className="bg-[#FFDBA0] py-2 px-4 rounded-3xl font-Manrope-semiBold text-black">
+              Find a house
+            </button>
+          </Link>
         </div>
         <div className="relative backdrop-brightness-200 backdrop-contrast-75 backdrop-blur-3xl flex flex-col gap-4 px-4 rounded-xl">
           <Image
@@ -71,34 +90,62 @@ export default function LandingPage() {
           </p>
         </div>
       </div>
-      <div className="relative px-60 py-7">
+      <div className="relative px-40 py-7">
         <div
           className="backdrop-brightness-200 backdrop-contrast-75 backdrop-blur-3xl flex items-center justify-between gap-10 p-4 rounded-lg
         "
         >
           <p className="flex flex-col gap-2">
             <span>Where</span>
-            <span>Search destinations</span>
+            <input
+              className="bg-transparent w-32"
+              type="text"
+              placeholder="Search destinations"
+              value={where}
+              onChange={(e) => setWhere(e.target.value)}
+            />
           </p>
+
           <div className="w-[2px] bg-white h-16"></div>
           <p className="flex flex-col gap-2">
-            <span>check in</span>
-            <span>add dates</span>
+            <span>Check In</span>
+            <input
+              className="bg-transparent w-32"
+              type="text"
+              placeholder="Add Dates"
+              value={checkIn}
+              onChange={(e) => setCheckIn(e.target.value)}
+            />
           </p>
           <div className="w-[2px] bg-white h-16"></div>
           <p className="flex flex-col gap-2">
             <span>check out</span>
-            <span>Add dates</span>
+            <input
+              className="bg-transparent w-32"
+              type="text"
+              placeholder="Add Dates"
+              value={checkOut}
+              onChange={(e) => setCheckOut(e.target.value)}
+            />
           </p>
           <div className="w-[2px] bg-white h-16"></div>
           <p className="flex flex-col gap-2">
             <span>Who</span>
-            <span>add guest</span>
+            <input
+              className="bg-transparent w-32"
+              type="text"
+              placeholder="Add Guests"
+              value={who}
+              onChange={(e) => setWho(e.target.value)}
+            />
           </p>
           <div className="w-[2px] bg-white h-16"></div>
-          <p className="flex flex-col gap-2 bg-[#FFDBA0] py-4 px-6 rounded-lg">
+          <p
+            className="flex flex-col gap-2 items-center bg-[#FFDBA0] py-4 px-6 rounded-lg"
+            onClick={handleSearch}
+          >
             <Search />
-            <span>Search</span>
+            <span className="text-black font-Manrope-bold">Search</span>
           </p>
         </div>
       </div>
